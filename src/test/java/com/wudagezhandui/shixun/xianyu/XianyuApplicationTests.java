39fdc6e07f93b475b18bc2d43cf764738bca393b
XiaoHuaShiFu <827032783@qq.com>
2020-08-22 02:28:36 +0800

    1

---
+++ src/test/java/com/wudagezhandui/shixun/xianyu/XianyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wudagezhandui.shixun.xianyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XianyuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
